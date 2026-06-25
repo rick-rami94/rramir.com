ca1aaf564ba669363569c042ee0e7977f53b7e84
Rick Ramirez <rickramirez94@gmail.com>
2026-06-25 10:52:08 -0400

    Add social share image (OG) and sitemap
    - 1200x630 Open Graph / Twitter card image (public/og.png)
    - og:image + summary_large_image meta tags in the page head
    - @astrojs/sitemap integration generating sitemap-index.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build
 export default defineConfig({
   site: 'https://rramir.com',
   compressHTML: true,
+  integrations: [sitemap()],
 });
